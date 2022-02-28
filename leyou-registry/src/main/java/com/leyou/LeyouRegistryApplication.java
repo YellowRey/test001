76fd9385f8a2b6c31710dc0a91637ae091f8ffd0
huang_jun_xian <173723542@qq.com>
2022-02-28 11:24:29 +0800

    no.1-test

---
+++ leyou-registry/src/main/java/com/leyou/LeyouRegistryApplication.java
@@ -0,0 +1,17 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka服务器端的启动器
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LeyouRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouRegistryApplication.class);
+    }
+}
